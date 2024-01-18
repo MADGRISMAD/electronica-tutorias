858155229e825dfbfb8bba0032e88c3fa9b316fa
madeline <madgrismad@gmail.com>
2024-01-17 19:29:24 -0800

    Add styles and login functionality to App.vue component

--- frontend/tutorias-front-end/src/components/HelloWorld.vue
+++ frontend/tutorias-front-end/src/components/HelloWorld.vue
@@ -1,32 +1,58 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <!-- Container -->
+  <div class="flex flex-wrap min-h-screen w-full content-center justify-center bg-gray-200 py-10">
+
+    <!-- Login component -->
+    <div class="flex shadow-md">
+      <!-- Login form -->
+      <div class="flex flex-wrap content-center justify-center rounded-l-md bg-white"
+        style="width: 24rem; height: 32rem;">
+        <div class="w-72">
+          <!-- Heading -->
+          <h1 class="text-xl font-semibold">Bienvenido</h1>
+          <small class="text-gray-400">Ingresa tus datos para iniciar sesion</small>
+
+          <!-- Form -->
+          <form class="mt-4">
+            <div class="mb-3">
+              <label class="mb-2 block text-xs font-semibold">numero de control</label>
+              <input type="email" placeholder="numero de control"
+                class="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500" />
+            </div>
+
+            <div class="mb-3">
+              <label class="mb-2 block text-xs font-semibold">contraseña</label>
+              <input type="password" placeholder="*****"
+                class="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500" />
+            </div>
+
+            <div class="mb-3 flex flex-wrap content-center">
+              <input id="remember" type="checkbox" class="mr-1 checked:bg-purple-700" /> 
+              <a href="#" class="text-xs font-semibold text-purple-700">Olvidaste tu contraseña?</a>
+            </div>
+
+            <div class="mb-3">
+              <button
+                class="mb-1.5 block w-full text-center text-white bg-purple-700 hover:bg-purple-900 px-2 py-1.5 rounded-md">Inicia sesion</button>
+
+            </div>
+          </form>
+
+          <!-- Footer -->
+          <div class="text-center">
+            <span class="text-xs text-gray-400 font-semibold">No tienes una cuenta?</span>
+            <a href="#" class="text-xs font-semibold text-purple-700">Registrate</a>
+          </div>
+        </div>
+      </div>
+
+      <!-- Login banner -->
+      <div class="flex flex-wrap content-center justify-center rounded-r-md" style="width: 24rem; height: 32rem;">
+        <img class="w-full h-full bg-center bg-no-repeat bg-cover rounded-r-md" src="https://i.imgur.com/9l1A4OS.jpeg">
+      </div>
+
+    </div>
+
   </div>
 </template>
 
@@ -39,20 +65,4 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+
