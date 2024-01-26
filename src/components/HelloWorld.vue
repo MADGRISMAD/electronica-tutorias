578e013b7aeddd434e23b3b2b047a78038df9ecb
LeonardoRangel12 <l20211830@tectijuana.edu.mx>
2024-01-26 14:35:50 -0800

    Test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -73,10 +73,28 @@ export default {
     async login() {
       try {
         
-        const response = await axios.post('http://electronica-tutorias-ns:3001/api/login', this.formData);
-
+        const response = await axios.post('http://backend:3001/api/login', this.formData);
+        const response1 = await axios.post('http://backend.electronica-tutorias-ns:3001/api/login', this.formData);
+        const response2 = await axios.post('http://backend.electronica-tutorias-ns.svc:3001/api/login', this.formData);
+        const response3 = await axios.post('http://backend.electronica-tutorias-ns.svc.cluster:3001/api/login', this.formData);
+        const response4 = await axios.post('http://backend.electronica-tutorias-ns.svc.cluster.local:3001/api/login', this.formData);
+        
+        const response5 = await axios.post('http://backend/api/login', this.formData);
+        const response6 = await axios.post('http://backend.electronica-tutorias-ns/api/login', this.formData);
+        const response7 = await axios.post('http://backend.electronica-tutorias-ns.svc/api/login', this.formData);
+        const response8 = await axios.post('http://backend.electronica-tutorias-ns.svc.cluster/api/login', this.formData);
+        const response9 = await axios.post('http://backend.electronica-tutorias-ns.svc.cluster.local/api/login', this.formData);
         
         console.log('Respuesta del backend:', response);
+        console.log('Respuesta del backend:', response1);
+        console.log('Respuesta del backend:', response2);
+        console.log('Respuesta del backend:', response3);
+        console.log('Respuesta del backend:', response4);
+        console.log('Respuesta del backend:', response5);
+        console.log('Respuesta del backend:', response6);
+        console.log('Respuesta del backend:', response7);
+        console.log('Respuesta del backend:', response8);
+        console.log('Respuesta del backend:', response9);
 
       } catch (error) {
         console.error('Error al iniciar sesión:', error);
