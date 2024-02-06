372313ff957bde1be5448bac8116c34ff0435142
LeonardoRangel12 <l20211830@tectijuana.edu.mx>
2024-02-05 22:34:20 -0800

    Changed API URL

--- FrontEnd/src/const.js
+++ FrontEnd/src/const.js
@@ -1,6 +1,6 @@
 // CAMBIAR AL HACER PUSH
-// export const API_URL = '/api'
-export const API_URL = 'http://localhost:3001/api'
+export const API_URL = '/api'
+// export const API_URL = 'http://localhost:3001/api'
 export const AXIOS_CONFIG = {
     headers: {
         'Content-Type': 'multipart/form-data'
