a0c2d19dc500d7b321d528021c5763d684961f1d
LeonardoRangel12 <l20211830@tectijuana.edu.mx>
2024-02-15 22:01:38 -0800

    Added middleware to citas route

--- BackEnd/routes/cita.route.js
+++ BackEnd/routes/cita.route.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const { citaRegistro, getCitas, getCitaByAlumno } = require("../components/cita.component");
+const {
+    citaRegistro,
+    getCitas,
+    getCitaByAlumno,
+} = require("../components/cita.component");
+const verifySession = require("../middleware/verifySession");
+const { verifyAlumno, verifyMaestro } = require("../middleware/verifyType");
 const upload = multer();
 
-router.get("/", upload.none(), getCitas);
-router.get("/:id", upload.none(), getCitaByAlumno);
-router.put("/", upload.none(), citaRegistro);
+router.get("/", verifySession, verifyMaestro, upload.none(), getCitas);
+router.get("/:id", verifySession, verifyAlumno, upload.none(), getCitaByAlumno);
+router.put("/", verifySession, verifyAlumno, upload.none(), citaRegistro);
 
 module.exports = router;
