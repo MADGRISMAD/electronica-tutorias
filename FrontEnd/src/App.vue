b004a83da853de83d3e9383ebec4672ed67a2ed9
madeline <madgrismad@gmail.com>
2024-02-28 00:08:31 -0800

    Update dependencies and add router

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -1,50 +1,7 @@
 <template>
-  <div class="app">
-    <!-- Llama al componente del sidebar -->
-    <SidebarComponent @switchComponent="switchComponent" />
-
-    <!-- Contenido principal (área de preguntas frecuentes) -->
-    <div class="content">
-      <component :is="currentComponent" />
-    </div>
-  </div>
+  <RouterView />
 </template>
 
 <script>
-import SidebarComponent from './components/SidebarComponent.vue';
-import LoginComponent from './components/LoginComponent.vue';
-import RegisterComponent from './components/ResgisterComponent.vue';
-import MainPage from './components/MainPage.vue';
-import FaqComponent from './components/FaqComponent.vue';
 
-export default {
-  name: 'App',
-  components: {
-    SidebarComponent,
-    LoginComponent,
-    RegisterComponent,
-    MainPage,
-    FaqComponent,
-  },
-  data() {
-    return {
-      currentComponent: 'FaqComponent',
-    };
-  },
-  methods: {
-    switchComponent(componentName) {
-      this.currentComponent = componentName;
-    },
-  },
-};
 </script>
-
-<style>
-.app {
-  display: flex;
-}
-
-.content {
-  flex: 1;
-}
-</style>
--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -1,6 +1,23 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router';
+
 import App from './App.vue'
 import './index.css'
+import LoginComponent from './components/LoginComponent.vue';
+import RegisterComponent from './components/ResgisterComponent.vue';
+import MainPage from './components/MainPage.vue';
+import FaqComponent from './components/FaqComponent.vue';
+
+const routes = [
+    { path: '/login', component: LoginComponent },
+    { path: '/register', component: RegisterComponent },
+    { path: '/faq', component: FaqComponent },
+    { path: '/', component: MainPage }
+]
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
 
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
